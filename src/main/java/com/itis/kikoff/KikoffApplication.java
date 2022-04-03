fd3367e588d313bd6258f2fd6217b5139b7cac9a
AksanovK <aksanovkamil@gmail.com>
2022-04-03 22:37:12 +0300

    1

---
+++ src/main/java/com/itis/kikoff/KikoffApplication.java
@@ -0,0 +1,13 @@
+package com.itis.kikoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KikoffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KikoffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itis/kikoff/KikoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itis.kikoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KikoffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
