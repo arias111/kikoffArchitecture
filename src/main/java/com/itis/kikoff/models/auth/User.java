061966994971b3ae11c58293fba57ac7eceab562
Govtatar\Ilzira.Ahmetdinova <Ilzira.Ahmetdinova@tatar.ru>
2022-04-07 00:12:27 +0300

    creating auth

--- src/main/java/com/itis/kikoff/models/auth/User.java
+++ src/main/java/com/itis/kikoff/models/auth/User.java
@@ -1,13 +1,22 @@
 package com.itis.kikoff.models.auth;
 
 import com.itis.kikoff.models.enums.Role;
+import com.itis.kikoff.models.enums.State;
 import com.itis.kikoff.models.payments.PersonalAccount;
 import com.itis.kikoff.models.shop.Basket;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "account")
 public class User {
@@ -19,12 +28,19 @@ public class User {
     private String firstName;
     private String lastName;
     private String patronymic;
+
     @Enumerated(value = EnumType.STRING)
     private Role role;
+
+    @Enumerated(value = EnumType.STRING)
+    private State state;
+
     private LocalDateTime birthday;
     private LocalDateTime creationDate;
     @OneToMany(mappedBy = "user")
     private List<Basket> baskets;
     @OneToOne(mappedBy = "user")
     private PersonalAccount personalAccount;
+
+    private String confirmCode;
 }
--- src/main/java/com/itis/kikoff/repositories/UserRepository.java
+++ src/main/java/com/itis/kikoff/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.itis.kikoff.repositories;
 import com.itis.kikoff.models.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional <User> findByConfirmCode(String confirmCode);
 }
