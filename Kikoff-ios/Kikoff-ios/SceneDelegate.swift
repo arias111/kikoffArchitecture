1bc46c407db88796a2b0c871eac552562610b553
Ruslan <akhaman@yandex.ru>
2022-04-18 20:51:20 +0300

    Setup environment

---
+++ Kikoff-ios/Kikoff-ios/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Kikoff-ios
+//
+//  Created by Руслан Ахмадеев on 18.04.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Kikoff-ios/Kikoff-ios/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Kikoff-ios
+//
+//  Created by Руслан Ахмадеев on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
