6c8dd6bc0a1d14d69a97e834968299fe2a884c42
AksanovK <aksanovkamil@gmail.com>
2022-05-12 00:58:25 +0300

    add logout method

--- src/main/java/com/itis/kikoff/controllers/SignInController.java
+++ src/main/java/com/itis/kikoff/controllers/SignInController.java
@@ -6,9 +6,7 @@ import com.itis.kikoff.services.SignInService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api("signInController")
@@ -21,4 +19,10 @@ public class SignInController {
     public ResponseEntity<TokenDto> signIn(@RequestBody EmailPasswordDto emailPassword) throws Throwable {
         return ResponseEntity.ok(signInService.signIn(emailPassword));
     }
+
+    @GetMapping("/logout")
+    public ResponseEntity<?> signIn(@RequestHeader("X-TOKEN") String token) throws Throwable {
+        return ResponseEntity.ok("");
+    }
+
 }
